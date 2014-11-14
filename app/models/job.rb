beb93d4326c60ea1055bbde5608f0e5280ef7801
cgaliber <cyril.galibern@free.fr>
2014-11-14 23:55:10 +0100

    explain when no service exists

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,4 @@
 class Job < ActiveRecord::Base
+	validates_presence_of :service_id
 	belongs_to :services
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -1,3 +1,4 @@
 class Service < ActiveRecord::Base
 	has_many :jobs
+	[ :svcname, :status].each { |col| validates_presence_of col }
 end
